07cf8e85728b029d1f623d20d160b11c610f20e5
Julie Qiu <julie@golang.org>
2022-12-27 17:15:25 -0500

    internal/mycopy: update to accept custom path

--- cmd/mycopy/main.go
+++ cmd/mycopy/main.go
@@ -11,24 +11,26 @@ import (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "usage: copy [new-repo] [old-repo] [dir]\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: copy [new-repo] [new-dir] [old-repo] [old-dir]\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "Copy a package inside a Go repo to x/metrics.\n")
-		fmt.Fprintf(flag.CommandLine.Output(), "  new-repo: name of the current working repo\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  new-repo: name of the repo to copy from, for example, pkgsite\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  new-dir: name of the directory inside the repo to copy from, for example, internal/fetch\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "  old-repo: name of the repo to copy from, for example, pkgsite\n")
-		fmt.Fprintf(flag.CommandLine.Output(), "  dir: name of the directory inside the repo to copy from, for example, internal/fetch\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  old-: name of the directory inside the repo to copy from, for example, internal/fetch\n")
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
-	if flag.NArg() != 3 {
+	if flag.NArg() != 4 {
 		flag.Usage()
 		os.Exit(1)
 	}
 	newRepo := flag.Args()[0]
-	oldRepo := flag.Args()[1]
-	dir := flag.Args()[2]
+	newDir := flag.Args()[1]
+	oldRepo := flag.Args()[2]
+	oldDir := flag.Args()[3]
 
-	if err := mycopy.Run(newRepo, oldRepo, dir); err != nil {
+	if err := mycopy.Run(newRepo, newDir, oldRepo, oldDir); err != nil {
 		log.Fatal(err)
 	}
 }
--- internal/mycopy/mycopy.go
+++ internal/mycopy/mycopy.go
@@ -14,11 +14,11 @@ import (
 	"github.com/julieqiu/derrors"
 )
 
-func Run(newRepo, oldRepo, dir string) error {
-	if err := validateRequest(oldRepo, dir); err != nil {
+func Run(newRepo, newDir, oldRepo, oldDir string) error {
+	if err := validateRequest(oldRepo, oldDir); err != nil {
 		return err
 	}
-	return copyAndEdit(newRepo, oldRepo, dir)
+	return copyAndEdit(newRepo, newDir, oldRepo, oldDir)
 }
 
 // validateRequest checks if repo and dir is a valid Go project repository and
@@ -48,17 +48,13 @@ func goDirectoryCommitURL(repo, dir, commit string) string {
 	return fmt.Sprintf("%s/+/%s/%s", goRepoURL(repo), commit, dir)
 }
 
-func modulePath(repo string) string {
-	return fmt.Sprintf("golang.org/x/%s", repo)
-}
-
-func packagePath(repo, dir string) string {
-	return modulePath(repo) + "/" + dir
+func importPath(suffix string) string {
+	return fmt.Sprintf("golang.org/x/%s", suffix)
 }
 
 // copyAndEdit makes a copy of the package at
 // https://go.googlesource.com/<repo>/+/refs/heads/master/<dir> into <dir>.
-func copyAndEdit(newRepo, oldRepo, dir string) (err error) {
+func copyAndEdit(newRepo, newDir, oldRepo, oldDir string) (err error) {
 	tempDir, err := ioutil.TempDir("", "go_")
 	if err != nil {
 		return err
@@ -77,7 +73,7 @@ func copyAndEdit(newRepo, oldRepo, dir string) (err error) {
 	}
 
 	// Copy package into <dir>.
-	cmd = exec.Command("cp", "-r", fmt.Sprintf("%s/%s", tempDir, dir), dir)
+	cmd = exec.Command("cp", "-r", fmt.Sprintf("%s/%s", tempDir, oldDir), newDir)
 	log.Println(strings.Join(cmd.Args, " "))
 	if err = cmd.Run(); err != nil {
 		return fmt.Errorf("cmd.Run(%q): %v", strings.Join(cmd.Args, " "), err)
@@ -85,13 +81,13 @@ func copyAndEdit(newRepo, oldRepo, dir string) (err error) {
 
 	// Edit files to replace all "internal" paths and add header about origin.
 	commit := commitForMaster(oldRepo)
-	return filepath.Walk(dir, func(filename string, info os.FileInfo, err error) error {
+	return filepath.Walk(oldDir, func(filename string, info os.FileInfo, err error) error {
 		fileInfo, err := os.Stat(filename)
 		if err != nil {
 			log.Fatalf("os.Stat(%q): %v", filename, err)
 		}
 		if !fileInfo.IsDir() && filepath.Ext(filename) == ".go" {
-			if err := editFile(filename, newRepo, oldRepo, dir, commit); err != nil {
+			if err := editFile(filename, newRepo, newDir, oldRepo, oldDir, commit); err != nil {
 				return err
 			}
 		}
@@ -102,8 +98,8 @@ func copyAndEdit(newRepo, oldRepo, dir string) (err error) {
 // editFile prepends the file with a message to warn users not
 // make any edits to the file. It also finds occurrences of "<repo>/internal/<dir>"
 // and replaces them with "<new-repo>/internal/<dir>".
-func editFile(filename, newRepo, oldRepo, dir, commit string) (err error) {
-	defer derrors.Wrap(&err, "editFile(%q, %q, %q, %q, %q)", filename, newRepo, oldRepo, dir, commit)
+func editFile(filename, newRepo, newDir, oldRepo, oldDir, commit string) (err error) {
+	defer derrors.Wrap(&err, "editFile(%q, %q, %q, %q, %q, %q)", filename, newRepo, newDir, oldRepo, oldDir, commit)
 	log.Printf("Editing: %s", filename)
 
 	// Create a temporary file for writing. This will be renamed at the end of the function.
@@ -115,7 +111,7 @@ func editFile(filename, newRepo, oldRepo, dir, commit string) (err error) {
 
 	writer := bufio.NewWriter(wf)
 	fmt.Fprintln(writer, "// DO NOT EDIT. This file was copied from")
-	fmt.Fprintln(writer, fmt.Sprintf("// %s", goDirectoryCommitURL(oldRepo, dir, commit)))
+	fmt.Fprintln(writer, fmt.Sprintf("// %s", goDirectoryCommitURL(oldRepo, oldDir, commit)))
 	fmt.Fprintln(writer, "")
 
 	contents, err := readLines(filename)
@@ -123,10 +119,12 @@ func editFile(filename, newRepo, oldRepo, dir, commit string) (err error) {
 		return err
 	}
 	pkgToDownload := map[string]bool{}
+	internalPath := importPath(oldRepo + "/internal")
 	for _, line := range contents {
-		if strings.Contains(line, modulePath(oldRepo)+"/internal") {
+		if strings.Contains(line, internalPath) {
 			pkgToDownload[strings.Fields(line)[0]] = true
-			line = replaceImportPath(line, newRepo, oldRepo)
+			line = replaceImportPath(line, importPath(newRepo+"/"+newDir), internalPath)
+			fmt.Println(line)
 		}
 		fmt.Fprintln(writer, fmt.Sprintf("%s", line))
 	}
@@ -134,16 +132,11 @@ func editFile(filename, newRepo, oldRepo, dir, commit string) (err error) {
 	if err := writer.Flush(); err != nil {
 		return err
 	}
-	fmt.Printf("%s depends on the following internal packages:\n", oldRepo)
-	for pkg := range pkgToDownload {
-		fmt.Printf("\t%s\n", pkg)
-	}
 	return os.Rename(fmt.Sprintf("%s_tmp", filename), filename)
 }
 
-func replaceImportPath(line, newRepo, oldRepo string) string {
-	parts := strings.Split(line, modulePath(oldRepo))
-	return strings.Join(parts, modulePath(newRepo))
+func replaceImportPath(line, newPath, oldPath string) string {
+	return strings.ReplaceAll(line, oldPath, newPath)
 }
 
 // readLines reads the contents of filename and returns each line of the file.
